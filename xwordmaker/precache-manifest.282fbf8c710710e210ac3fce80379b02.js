445593af50976996ceb513d7ab01c42f9efc2e8c
Keith Laban <kelaban17@gmail.com>
2019-10-24 21:58:51 -0400

    add xwordmaker

---
+++ xwordmaker/precache-manifest.282fbf8c710710e210ac3fce80379b02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258f6a2de0c884f612142644cb05c99f",
+    "url": "/xwordmaker/index.html"
+  },
+  {
+    "revision": "8ffc06e31a1e8b2a3ab5",
+    "url": "/xwordmaker/static/css/main.020e7b1e.chunk.css"
+  },
+  {
+    "revision": "722173c047d85e76b5b4",
+    "url": "/xwordmaker/static/js/2.2d56d6f0.chunk.js"
+  },
+  {
+    "revision": "8ffc06e31a1e8b2a3ab5",
+    "url": "/xwordmaker/static/js/main.890b2726.chunk.js"
+  },
+  {
+    "revision": "1dc39b8bc1599b98680f",
+    "url": "/xwordmaker/static/js/runtime-main.e4fdeecd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/xwordmaker/static/media/logo.25bf045c.svg"
+  }
+]);
